74b35aba959f665f1920e7a88aad14fb8d15f931
Pedro4064 <pedrohlcruz@gmail.com>
2025-11-03 20:58:20 -0300

    [IMPLEMENTED] Filter on items not read on rss feed

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,42 @@ mod qbittorrent;
 
 use std::path::Path;
 
-use crate::config_loader::{Config, ConfigLoader};
+use crate::{
+    config_loader::{Config, ConfigLoader},
+    qbittorrent::rss_feed::Article,
+};
 fn main() {
     let config_path = Path::new("/home/pedro-cruz/Projects/picky_feed/picky_conf.toml");
     let conf_loader = ConfigLoader::new(config_path);
     let configuration = conf_loader.parse_config();
     println!("{:?}", configuration);
 
-    let mut torrent = qbittorrent::QBitTorrentClient::new(configuration.credentials.user_name, configuration.credentials.user_password, "http://localhost:8080".to_string());
-    torrent.auth_user().expect("Error in user auth, unable to proceed");
-    let rss_feeds = torrent.get_rss_items().expect("Error parsing rss feeds, unable to proceed");
-    println!("{:?}", rss_feeds);
+    let mut torrent = qbittorrent::QBitTorrentClient::new(
+        configuration.credentials.user_name,
+        configuration.credentials.user_password,
+        "http://localhost:8080".to_string(),
+    );
+    torrent
+        .auth_user()
+        .expect("Error in user auth, unable to proceed");
+    let rss_feeds = torrent
+        .get_rss_items()
+        .expect("Error parsing rss feeds, unable to proceed");
+
+    for rss_feed_config in configuration.rss_rules {
+        let rss_feed_items = match rss_feeds.get(&rss_feed_config.rss_name) {
+            Some(items) => items,
+            None => {
+                println!("No rss feed found with name {}", &rss_feed_config.rss_name);
+                continue;
+            }
+        };
+
+        let rss_unread_items: Vec<&Article> = rss_feed_items
+            .articles
+            .iter()
+            .filter(|item| item.is_read)
+            .collect();
+        println!("{:?}", rss_unread_items);
+    }
 }
--- src/qbittorrent/rss_feed.rs
+++ src/qbittorrent/rss_feed.rs
@@ -3,14 +3,14 @@ use serde::Deserialize;
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Article{
-    id: String,
+    pub id: String,
     #[serde(rename="torrentURL")]
-    torrent_url: String,
+    pub torrent_url: String,
     #[serde(default)]
-    is_read: bool,
+    pub is_read: bool,
 }
 
 #[derive(Debug, Deserialize)]
 pub struct Feed{
-    articles: Vec<Article>,
+    pub articles: Vec<Article>,
 }
